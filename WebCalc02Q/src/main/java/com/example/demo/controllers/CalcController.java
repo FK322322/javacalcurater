b3c0a20c950683130864d1d123c3348ccb03e7da
FK322322 <115530679+FK322322@users.noreply.github.com>
2022-10-12 15:03:19 +0900

    Update CalcController.java
    数字以外が入力されたときにres2.htmlに画面遷移するように変更

--- WebCalc02Q/src/main/java/com/example/demo/controllers/CalcController.java
+++ WebCalc02Q/src/main/java/com/example/demo/controllers/CalcController.java
@@ -14,18 +14,22 @@ public class CalcController {
 	@Autowired
 	private CalcService service;
 
+	
+
+	
 	@PostMapping("resAdd")
-	public String resAdd(
+	public String resAdd	(
 			Model model,
 			@RequestParam("numA") String NumA,
 			@RequestParam("numB") String NumB
-			
-		
 
 	) {
 
-		model.addAttribute("kotae", service.calculateAdd(NumA, NumB));
-
+		try {
+			model.addAttribute("kotae", service.calculateAdd(NumA, NumB));
+		} catch (Exception e) {
+			return "res2";
+		}
 		return "res";
 
 	}
@@ -39,8 +43,11 @@ public class CalcController {
 	
 				) {
 
-			model.addAttribute("kotae", service.calculatesub(NumA, NumB));
-
+			try {
+				model.addAttribute("kotae", service.calculatesub(NumA, NumB));
+			} catch (Exception e) {
+				return "res2";
+			}
 			return "res";
 	
 
@@ -56,8 +63,11 @@ public class CalcController {
 	
 				) {
 
-			model.addAttribute("kotae", service.calculatemul(NumA, NumB));
-
+			try {
+				model.addAttribute("kotae", service.calculatemul(NumA, NumB));
+			} catch (Exception e) {
+				return "res2";
+			}
 			return "res";
 	
 
@@ -74,17 +84,17 @@ public class CalcController {
 	
 				) {
 
-			model.addAttribute("kotae", service.calculatediv(NumA, NumB));
-				
+			try {
+				model.addAttribute("kotae", service.calculatediv(NumA, NumB));
+			} catch (Exception e) {
+				return "res2";
+			}
 			return "res";
 	
 
-	
 	}
-
+}
 	//	引き算、掛け算、割り算を記載していきましょう。
 	//	ですが、割り算はちょっと特殊なので注意しましょう！
 	//	割り算の答えの出し方をよく思い出してくださいね、他の掛け算までの答え方とは別で回答パターンが複数あると思います、
 	//	それを意識してコードを書いていってください。
-
-}
